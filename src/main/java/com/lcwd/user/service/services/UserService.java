9a56b326518b268a190920c48d87f560d148db2c
deepakshraddha <144250748+deepakshraddha@users.noreply.github.com>
2023-09-07 01:08:56 +0530

    Initial commit

---
+++ src/main/java/com/lcwd/user/service/services/UserService.java
@@ -0,0 +1,18 @@
+package com.lcwd.user.service.services;
+
+import com.lcwd.user.service.entities.User;
+
+import java.util.List;
+
+public interface UserService {
+    // user operations
+    //create user
+    User saveUser(User user);
+    //get All User
+    List<User> getAllUser();
+    //get single user of given userId
+    User getUser(String userId);
+
+    //TODO : delete
+    //Todo : Update
+}
